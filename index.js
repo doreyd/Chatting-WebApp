6d414145f43ff26a3b3db628729a24c896822832
Doreyd Mehila <dmehila@gmail.com>
2019-04-29 13:04:31 +0200

    Added a route for users to sign in

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Signing up a new user and adding it to the db
+// Signing up a new user and adding it to the database
 app.post("/signUp", (req, res, next) => {
   UserRecord.find({ email: req.body.email }, (err, data) => {
     if (data.length !== 0)
@@ -23,14 +23,28 @@ app.post("/signUp", (req, res, next) => {
       });
       newUser
         .save()
-        .then(() => {
-          res.status(201).json({ message: "user added" });
-        })
-        .catch(err => {
-          res.status(500).json({ error: err });
-        });
+        .then(() => res.status(201).json({ message: "user added" }))
+        .catch(err => res.status(500).json({ error: err }));
     }
   });
+});
+
+// Singin In as a user
+app.post("/signIn", (req, res, next) => {
+  UserRecord.find(
+    { email: req.body.email, password: req.body.password },
+    (err, data) => {
+      if (data.length === 0)
+        res
+          .status(500)
+          .json({ message: "email/password combination not in database" });
+      else {
+        res
+          .status(200)
+          .json({ allMessage: data[0].allMessage, login: data[0].login });
+      }
+    }
+  );
 });
 
 app.get("/", (req, res, next) => {
