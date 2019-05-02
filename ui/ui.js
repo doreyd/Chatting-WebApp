2e50bbb06624f438796bd62fd1a1921b95a297b1
Doreyd Mehila <dmehila@gmail.com>
2019-05-02 21:06:49 +0200

    Refactored the initial code to make it more dynamic

--- ui/ui.js
+++ ui/ui.js
@@ -29,39 +29,50 @@ const stateChange = (messages, type, state) => {
 
 console.log(stateCount(data["kayla"], "sender", false));
 
-const $kayla = document.getElementById("kayla");
-const $dawn = document.getElementById("dawn");
-const $john = document.getElementById("john");
-const $myself = document.getElementById("myself");
+let users = [
+  ["kayla", 100, 100, 6],
+  ["john", 400, 400, 3],
+  ["dawn", 200, 500, 2],
+  ["myself", 300, 300, 0]
+];
 
-let elems = [$kayla, $john, $dawn, $myself];
+// Dynamic selection of dom elements from users array
+let elems = users.map(x => document.getElementById(x[0]));
+
+// let colorBase = "#3749ce";
+let colorBase = "steelblue";
+let dim = 80;
+let dimC = 20;
+// let msgQty = 6;
 
 const setUsers = () => {
-  elems.forEach(elem => {
-    // let dim = Math.round(Math.random() * 200);
-    dim = 80;
+  elems.forEach((elem, i) => {
     elem.style.position = "absolute";
-    elem.style.top = Math.round(Math.random() * 600) + "px";
-    elem.style.left = Math.round(Math.random() * 1000) + "px";
+    // elem.style.top = Math.round(Math.random() * 600) + "px";
+    // elem.style.left = Math.round(Math.random() * 1000) + "px";
+    elem.style.top = users[i][1] + "px";
+    elem.style.left = users[i][2] + "px";
     elem.style.height = dim + "px";
     elem.style.width = dim + "px";
     elem.style.borderRadius = "50%";
-    elem.style.border = "4px solid steelblue";
+    elem.style.border = "4px solid " + colorBase;
     elem.style.padding = "2px";
-    setMsgQt(dim, elem.style.top, elem.style.left);
-    setName(dim, elem.style.top, elem.style.left, "john");
+    elem.src = users[i][0] + ".jpg";
+    setName(elem.style.top, elem.style.left, users[i][0]);
+    if (users[i][3] !== 0) {
+      setMsgQt(elem.style.top, elem.style.left, users[i][3]);
+    }
   });
 };
 
-const setMsgQt = (dim, top, left) => {
+const setMsgQt = (top, left, msgQty) => {
   let circle = document.createElement("div");
-  let dimC = 20;
   circle.style.position = "absolute";
   circle.style.height = dimC + "px";
   circle.style.width = dimC + "px";
   circle.style.borderRadius = "50%";
   circle.style.background = "#e4e4e2";
-  circle.style.color = "steelblue";
+  circle.style.color = colorBase;
   cy = parseInt(top) + dim / 2 - (dim * 1.414) / 4;
   cx = parseInt(left) + dim / 2 + (dim * 1.414) / 4;
 
@@ -73,12 +84,12 @@ const setMsgQt = (dim, top, left) => {
   circle.style.textAlign = "center";
   circle.style.verticalAlign = "middle";
   circle.style.padding = "2px";
-  circle.innerText = "15";
+  circle.innerText = msgQty;
 
   document.body.append(circle);
 };
 
-const setName = (dim, top, left, name) => {
+const setName = (top, left, name) => {
   let nameDiv = document.createElement("div");
   nameDiv.style.position = "absolute";
   nameDiv.style.top = parseInt(top) + dim + "px";
@@ -89,7 +100,7 @@ const setName = (dim, top, left, name) => {
   nameDiv.style.textAlign = "center";
   nameDiv.style.verticalAlign = "middle";
   nameDiv.style.background = "#e4e4e2";
-  nameDiv.style.color = "steelblue";
+  nameDiv.style.color = colorBase;
   nameDiv.style.borderRadius = "5px";
   nameDiv.style.padding = "2px 5px 2px 5px";
   nameDiv.innerText = name;
@@ -98,3 +109,8 @@ const setName = (dim, top, left, name) => {
 };
 
 setUsers();
+
+// set dimensions as dim
+// set position as top, left
+// set number of messages as msgQty
+// set name of user as name
